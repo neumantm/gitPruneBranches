893c9b40a33f66443954bcf6a4311fe24151c89b
Tim Neumann <neumantm@fius.informatik.uni-stuttgart.de>
2023-02-20 16:45:12 +0100

    feat(install): Add nix flake (#3)

---
+++ flake.nix
@@ -0,0 +1,28 @@
+{
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs?ref=nixpkgs-unstable";
+    flake-utils.url = "github:numtide/flake-utils";
+  };
+
+  outputs = { self, nixpkgs, flake-utils, ... }: flake-utils.lib.eachDefaultSystem (system:
+    let
+      pkgs = import nixpkgs { inherit system; };
+    in
+    {
+      packages.default = with pkgs; stdenv.mkDerivation {
+        name = "gitPruneBranches";
+        src = self;
+        buildInputs = [ bash git ];
+        nativeBuildInputs = [ makeWrapper ];
+        installPhase = ''
+          mkdir -p $out/bin
+          cp gitPruneBranches.sh $out/bin/git-pruneBranches
+          chmod +x $out/bin/git-pruneBranches
+          wrapProgram $out/bin/git-pruneBranches \
+            --prefix PATH : ${lib.makeBinPath [ bash git ]}
+        '';
+        meta.mainProgram = "git-pruneBranches";
+      };
+    }
+  );
+}
